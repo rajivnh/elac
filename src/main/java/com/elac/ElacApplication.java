f78f38aba77e1c0241690f0c05b61af4fc88ac29
Rajiv Kumbhakar <rajivnh@yahoo.com>
2023-07-23 22:53:57 +0530

    first commit

---
+++ src/main/java/com/elac/ElacApplication.java
@@ -0,0 +1,13 @@
+package com.elac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elac/ElacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
